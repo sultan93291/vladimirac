20046ba176b80fa723145cde4034634f5853f9a8
fahim-sarker <sarkerfahim599@gmail.com>
2025-05-24 16:58:08 +0600

    remove unnecessary files

--- src/app/about-us/components/Team.tsx
+++ src/app/about-us/components/Team.tsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import { RiTwitterXLine, RiLinkedinBoxLine } from "react-icons/ri";
+import { RiTwitterXLine } from "react-icons/ri";
 import { FaLinkedinIn } from "react-icons/fa";
 
 const teamMembers = [
